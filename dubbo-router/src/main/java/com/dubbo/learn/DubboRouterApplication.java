c6a7b04ee67d5f3e78634239fde181a54933246d
270129219 <ght270129219@163.com>
2018-04-17 09:34:13 +0800

    IDEA整合GitHub测试

---
+++ dubbo-router/src/main/java/com/dubbo/learn/DubboRouterApplication.java
@@ -0,0 +1,12 @@
+package com.dubbo.learn;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class DubboRouterApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(DubboRouterApplication.class, args);
+	}
+}
